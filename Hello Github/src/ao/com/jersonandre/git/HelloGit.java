264f229a2ba3bc30020495f4c01805529cd83264
Jerson André <116963238+JersonJesus@users.noreply.github.com>
2024-06-11 10:02:38 +0100

    Adicionei arquivos via upload

---
+++ Hello Github/src/ao/com/jersonandre/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ao.com.jersonandre.git;
+
+/**
+ * 
+ * Exemplo de integração com o Eclipse com o Github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
